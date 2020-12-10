ffe4e351946613408289db45a78d4aeaea64f8a9
Roy Mosby <ego.madking@gmail.com>
2020-12-10 13:29:09 -0500

    commit tree 9041c92a8df551488425a3f2c8b05632a789bf64

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
+collection = ['ruby', 'javascript', 'python', 'objective-c']
 
+def my_collect(collection)
+  collection.map do |item|
+    yield item if block_given?
+  end
+end
 
+my_collect(collection){ |lang| lang.upcase}
